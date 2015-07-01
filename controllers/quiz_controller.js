8ae6bbce13dc808acc319ab8c6bbc5cb92e4b42e
Pepe <lynxpardina@2pe.org>
2015-07-01 12:42:43 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,20 @@
+// GET /quizes/question
+exports.question = function(req, res){
+  res.render('quizes/question', {pregunta: 'Capital de Italia'});
+}
+
+
+// GET /quizes/answer
+exports.answer = function(req, res) {
+
+ //if (!respuesta.match(/^ *lisboa *$/i)) resultado = "Respuesta " + respuesta + " incorrecta, la respuesta correcta es Lisboa";
+
+  if (req.query.respuesta.match (/^ *roma *$/i)){
+            // busca roma con o sin espacios al principio y al final
+            // ignorando las mayusculas y con $ comprueba que roma
+            // no forme parte de una palabra
+    res.render('quizes/answer', {respuesta: 'Correcto'});
+  }else {
+    res.render('quizes/answer', {respuesta: 'Incorrecto'});
+  }
+};
