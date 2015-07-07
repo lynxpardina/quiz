34dd506cb27489b96b7eee392242f42b47100245
Pepe <lynxpardina@2pe.org>
2015-07-07 20:07:08 +0200

    Adicion de multiples preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,26 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res){
-models.Quiz.findAll().success(function(quiz){
-  res.render('quizes/question', {title: 'Quiz', pregunta: quiz[0].pregunta});
+//GET /quizes
+exports.index = function(req, res){
+	models.Quiz.findAll().then (function(quizes){
+		res.render('quizes/index.ejs', {quizes: quizes});
+	})
+};
+
+// GET /quizes/:id
+exports.show = function(req, res){
+models.Quiz.find(req.params.quizId).then(function(quiz){
+  res.render('quizes/show', {quiz: quiz });
 })
-}
+};
 
-
-// GET /quizes/answer
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-
-models.Quiz.findAll().success(function(quiz){
-  if (req.query.respuesta.toLowerCase() === quiz[0].respuesta.toLowerCase()){
-    res.render('quizes/answer', {title: 'Quiz', respuesta: 'Correcto'});
+models.Quiz.find(req.params.quizId).then(function(quiz){
+  if (req.query.respuesta.toLowerCase() === quiz.respuesta.toLowerCase()){
+    res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
   }else {
-    res.render('quizes/answer', {title: 'Quiz', respuesta: 'Incorrecto'});
+    res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
   }
 })
 };
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,17 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Bienvenido a Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
 
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Autor' });
 });
+
+// Definición de rutas de quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
 
 module.exports = router;
